663c34f69690db13b6923edf1f106451dad16c02
JOSE DARIO FLOREZ GOMEZ <jd.florezg1@sis.virtual.uniandes.edu.co>
2019-11-18 13:54:31 -0500

    ya

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+let idioma2 = ()=>{
+    return navigator.language;
+}
+
+let idioma = ()=>{
+    console.log(navigator.language);
+    if(navigator.language==="es"){
+        return localeEsMessages;
+    }
+    
+    else if(navigator.language==="en"){
+        return localeEnMessages;
+    }
+}
+
+ReactDOM.render(
+	<IntlProvider locale={idioma2()} messages = {idioma()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
